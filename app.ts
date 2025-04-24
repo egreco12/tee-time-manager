63e6339e8adfc1204184897c5c9ca91ed31e668e
Evan Greco <e.greco12@gmail.com>
2025-04-23 18:26:25 -0700

    test slashcommand works

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { Application } from "express";
 import {
   ButtonStyleTypes,
@@ -9,17 +10,58 @@ import {
 } from 'discord-interactions';
 
 const app: Application = express();
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+const PORT = process.env.PORT || '3000';
 
-// app.get()
+/**
+ * Interactions endpoint URL where Discord will send HTTP requests
+ * Parse request body and verifies incoming requests using discord-interactions package
+ */
+
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY!!), async (req, res): Promise<any> => {
+  // Interaction id, type and data
+  const { id, type, data } = req.body;
+
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "test" command
+    if (name === 'test') {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: `hello world!`,
+        },
+      });
+    }
+
+    console.error(`unknown command: ${name}`);
+    return res.status(400).json({ error: 'unknown command' });
+  }
+
+  console.error('unknown interaction type', type);
+  return res.status(400).json({ error: 'unknown interaction type' });
+});
 
 app
-  .listen(PORT, "localhost", function () {
+  .listen(Number(PORT), 'localhost', function () {
     console.log(`Server is running on port ${PORT}.`);
   })
-  .on("error", (err: any) => {
-    if (err.code === "EADDRINUSE") {
-      console.log("Error: address already in use");
+  .on('error', (err: any) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log('Error: address already in use');
     } else {
       console.log(err);
     }
