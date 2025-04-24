02f49c87c72c9c5874ee51d827c2dec043852d73
Evan Greco <e.greco12@gmail.com>
2025-04-23 17:51:11 -0700

    Add more packages and scaffolding

---
+++ app.ts
@@ -0,0 +1,26 @@
+import express, { Application } from "express";
+import {
+  ButtonStyleTypes,
+  InteractionResponseFlags,
+  InteractionResponseType,
+  InteractionType,
+  MessageComponentTypes,
+  verifyKeyMiddleware,
+} from 'discord-interactions';
+
+const app: Application = express();
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+
+// app.get()
+
+app
+  .listen(PORT, "localhost", function () {
+    console.log(`Server is running on port ${PORT}.`);
+  })
+  .on("error", (err: any) => {
+    if (err.code === "EADDRINUSE") {
+      console.log("Error: address already in use");
+    } else {
+      console.log(err);
+    }
+  });
