88139290d82b50a51959e478b706cd54f045502b
Evan Greco <e.greco12@gmail.com>
2025-04-23 20:30:08 -0700

    remove copypasta

--- src/test_command.ts
+++ src/test_command.ts
@@ -4,7 +4,6 @@ export async function testCommand(res: any){
   return res.send({
   type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
   data: {
-    // Fetches a random emoji to send from a helper function
     content: `hello world!`,
   },
 });
